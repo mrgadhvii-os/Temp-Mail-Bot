861a855d0ff77c9ae761092f292e2c1c7497d3a9
mrgadhvii-os <jaydevswebpannel@gmail.com>
2025-06-16 23:25:12 +0530

    Update subscription.service.ts

--- services/subscription.service.ts
+++ services/subscription.service.ts
@@ -1,10 +1,10 @@
-import { BOT_TOKEN, CHANNEL_USERNAME } from "../config/config.ts";
+import { BOT_TOKEN } from "../config/config.ts";
 
 export const SubscriptionService = {
   async checkSubscription(userId: number): Promise<boolean> {
     try {
       const response = await fetch(
-        `https://api.telegram.org/bot${BOT_TOKEN}/getChatMember?chat_id=${CHANNEL_USERNAME}&user_id=${userId}`
+        `https://api.telegram.org/bot${BOT_TOKEN}/getChatMember?chat_id=@ItsUGPro&user_id=${userId}`
       );
       const data = await response.json();
       return ["member", "administrator", "creator"].includes(data.result?.status);
